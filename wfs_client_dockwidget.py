a20455fbd8b46cafbe814f2ed13682cbe6907621
Samweli <smwltwesa6@gmail.com>
2018-03-07 14:58:20 -0700

    finished interface

--- wfs_client_dockwidget.py
+++ wfs_client_dockwidget.py
@@ -24,7 +24,13 @@
 import os
 
 from PyQt4 import QtGui, uic
-from PyQt4.QtCore import pyqtSignal
+from PyQt4.QtCore import *
+
+from PyQt4.QtGui import *
+from PyQt4.QtGui import (
+    QDialog,)
+
+import requests
 
 FORM_CLASS, _ = uic.loadUiType(os.path.join(
     os.path.dirname(__file__), 'wfs_client_dockwidget_base.ui'))
@@ -44,7 +50,70 @@ class WFSClientDockWidget(QtGui.QDockWidget, FORM_CLASS):
         # #widgets-and-dialogs-with-auto-connect
         self.setupUi(self)
 
+    def accept(self):
+        """Create an isochrone map and display it in QGIS."""
+        error_dialog_title = self.tr("Error")
+        try:
+            self.save_state()
+            self.require_input()
+
+            server_url= self.server.text()
+
+            QApplication.instance().setOverrideCursor(Qt.BusyCursor)
+
+            result = ""
+            clientServer = "http://localhost:5000"
+
+            # WFS 3.0  Requirement 5
+            # WFS 3.0  Requirement 6
+
+            result = requests.get(clientServer +'/?server=' + str(server_url))
+
+            result = result + "\n" + requests.get(clientServer + '/api?server=' + str(server_url))
+
+            result = result + "\n" + requests.get(clientServer +'/api/conformance?server=' + str(server_url))
+
+            result = result + "\n" + requests.get(clientServer +'/link?server=' + str(server_url))
+
+            result = result + "\n" + requests.get(clientServer +'/http1.1?server=' + str(server_url))
+
+            result = result + "\n" + requests.get(clientServer +'/etag?server=' + str(server_url))
+
+            self.textBrowser.setText(result);
+
+            # WFS 3.0 Requirement 7
+
+            # WFS 3.0 Requirement 30
+
+            # WFS 3.0 Requirement 2
+            # WFS 3.0 Requirement 3
+
+            QApplication.instance().setOverrideCursor(Qt.ArrowCursor)
+
+            self.done(QDialog.Accepted)
+
+        except Exception as exception:  # pylint: disable=broad-except
+            # noinspection PyCallByClass,PyTypeChecker,PyArgumentList
+            self.display_warning_message_box(
+                self, error_dialog_title, exception.message)
+            pass
+        finally:
+            dialog_title = self.tr("Success")
+
+
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
+
+    def display_warning_message_box(parent=None, title=None, message=None):
+        """
+        Display a warning message box.
+
+        :param title: The title of the message box.
+        :type title: str
+
+        :param message: The message inside the message box.
+        :type message: str
+        """
+        QMessageBox.warning(parent, title, message)
 
