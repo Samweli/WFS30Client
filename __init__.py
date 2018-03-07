82299096d860014119d6e369928cb1a1e15c9089
Samweli <smwltwesa6@gmail.com>
2018-03-07 13:37:29 -0700

    initial commit

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ WFSClient
+                                 A QGIS plugin
+ Client for Web Feature Service 3.0
+                             -------------------
+        begin                : 2018-03-07
+        copyright            : (C) 2018 by Samweli Mwakisambwe
+        email                : smwltwesa6@gmail.com
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load WFSClient class from file WFSClient.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgisInterface
+    """
+    #
+    from .wfs_client import WFSClient
+    return WFSClient(iface)
